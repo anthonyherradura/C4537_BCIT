bb022a0de205cc0b0ba922bc8676ed94a5ff19bc
anthonyherradura <anthonyherradura604@gmail.com>
2026-09-11 15:38:16 -0700

    Change go button title case.

--- lang/messages/en/strings.js
+++ lang/messages/en/strings.js
@@ -2,5 +2,5 @@ export const STRINGS = {
     GAME_TITLE: "Button Memory Game",
     BTNS_CREATE_LABEL: "How many button to create?",
     INPUT_LABEL: "Enter a number between 3-7: ",
-    GO_BTN_TEXT: "GO"
+    GO_BTN_TEXT: "Go"
 };
