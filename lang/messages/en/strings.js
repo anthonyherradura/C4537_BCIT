4ea7597da9ad25c1ad9f3e2012237a4a856ca143
anthonyherradura <anthonyherradura604@gmail.com>
2026-09-11 14:48:06 -0700

    Commit made on PC

---
+++ lang/messages/en/strings.js
@@ -0,0 +1,6 @@
+export const STRINGS = {
+    GAME_TITLE: "Button Memory Game",
+    BTNS_CREATE_LABEL: "How many button to create?",
+    INPUT_LABEL: "Enter a number between 3-7: ",
+    GO_BTN_TEXT: "GO"
+};
