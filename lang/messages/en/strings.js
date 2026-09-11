91f12193fe3aef13b231ae0af3e83ca45226f70c
anthonyherradura <anthonyherradura604@gmail.com>
2026-09-11 16:17:55 -0700

    Added Button strings

--- lang/messages/en/strings.js
+++ lang/messages/en/strings.js
@@ -2,5 +2,12 @@ export const STRINGS = {
     GAME_TITLE: "Button Memory Game",
     BTNS_CREATE_LABEL: "How many button to create?",
     INPUT_LABEL: "Enter a number between 3-7: ",
-    GO_BTN_TEXT: "Go"
+    GO_BTN_TEXT: "Go",
+    BTN_1: "1",
+    BTN_2: "2",
+    BTN_3: "3",
+    BTN_4: "4",
+    BTN_5: "5",
+    BTN_6: "6",
+    BTN_7: "7"
 };
