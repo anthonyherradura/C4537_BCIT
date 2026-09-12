a5d6b7e2308dbf9a9217ffcd73835ae126dd8939
anthonyherradura <anthonyherradura604@gmail.com>
2026-09-11 17:21:56 -0700

    Adding instance variables to UserInterface.

--- js/classes/UserInterface.js
+++ js/classes/UserInterface.js
@@ -1,3 +1,12 @@
 class UserInterface {
+     constructor(rootElementId) {
+        this.root = document.getElementById(rootElementId);
+        this.header = null;
+        this.messageLabel = null;
+        this.inputLabel = null;
+        this.inputBox = null;
+        this.goButton = null;
+        this.statusMessage = null;
+     }
      
 }
