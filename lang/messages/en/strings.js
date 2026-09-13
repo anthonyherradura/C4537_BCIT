e520c503a917f970f76f5c5784e428ed06d33e3a
Anthony Herradura <anthonyherradura604@gmail.com>
2026-09-12 17:46:18 -0700

    Modify STRINGS object.

--- lang/messages/en/strings.js
+++ lang/messages/en/strings.js
@@ -1,13 +1,8 @@
-export const STRINGS = {
+export const STRINGS = Object.freeze({
     GAME_TITLE: "Button Memory Game",
     BTNS_CREATE_LABEL: "How many button to create?",
-    INPUT_LABEL: "Enter a number between 3-7: ",
+    ERROR_INVALID_NUMBER: "Please enter a number between 3 and 7.",
     GO_BTN_TEXT: "Go",
-    BTN_1: "1",
-    BTN_2: "2",
-    BTN_3: "3",
-    BTN_4: "4",
-    BTN_5: "5",
-    BTN_6: "6",
-    BTN_7: "7"
-};
+    MSG_EXCELLENT: "Excellent Memory!",
+    MSG_WRONG_ORDER: "Wrong Order!"
+});
